d80a2648426899206d1ab8abb23e5cc3e3cecb74
rodneyxr <rodneyxr@gmail.com>
2015-04-20 21:41:15 -0500

    finished!!

--- src/core/Cache.java
+++ src/core/Cache.java
@@ -5,14 +5,12 @@ public class Cache extends BaseCache {
 	private int lastTag;
 
 	private int lastBlockAddress;
-	private int lastEntryTag;
 	private MemoryAddress lastAddress;
 	private MemoryAddress[] lastAddressList;
 
 	public Cache(int cacheSize, int blockSize, int associativity, ReplacementPolicy replacementPolicy) {
 		super(cacheSize, blockSize, associativity, replacementPolicy);
 		lastTag = EMPTY;
-		lastEntryTag = EMPTY;
 	}
 
 	@Override
@@ -28,58 +26,77 @@ public class Cache extends BaseCache {
 		// and we will be modifying this array after this line
 		lastAddressList = tmpList.clone();
 
-		if (tmpList[0] != null)
-			lastEntryTag = getTag(tmpList[0]);
-		else
-			lastEntryTag = EMPTY;
+		int lastEntryTag = isHit(tmpList, address);
 
-		if (lastEntryTag == EMPTY) {
+		// set is empty
+		if (tmpList[0] == null) {
 			address.setLastAccessTime(accesses);
 			tmpList[0] = address;
 			miss();
-		} else if (lastEntryTag == lastTag) {
-			// TODO: search for address if not in there then miss?
-			if (RP == ReplacementPolicy.LRU) {
-				address.setLastAccessTime(accesses);
-				tmpList[0] = address;
-				lastAddressList[0] = address;
-			} else if (RP == ReplacementPolicy.FIFO) {
-				// lastAddressList[0]
-			}
-			hit();
-		} else {
-			address.setLastAccessTime(accesses);
-			boolean isFull = tmpList[ASSOCIATIVITY - 1] != null;
-			int index = 0;
 
-			if (!isFull) {
-				// find first empty block and set replace index
-				for (int i = 0; i < ASSOCIATIVITY; i++) {
-					if (tmpList[i] == null) {
-						index = i;
-						break;
-					}
-				}
-				// find oldest access time if full
-				// else put into first empty block
-			} else if (RP == ReplacementPolicy.LRU) {
-				// replace last used
-				for (int i = 0; i < ASSOCIATIVITY; i++) {
-					if (tmpList[i].getLastAccessTime() > tmpList[index].getLastAccessTime()) {
-						index = i;
-					}
-				}
-			} else if (RP == ReplacementPolicy.FIFO) {
-				// find oldest block and set replace index
-				for (int i = 0; i < ASSOCIATIVITY; i++) {
-					if (tmpList[i].getLastAccessTime() > tmpList[index].getLastAccessTime()) {
-						index = i;
-					}
+			// set is not empty but there was a miss
+		} else if (lastEntryTag == EMPTY) {
+			address.setLastAccessTime(accesses);
+			replace(tmpList, address);
+			miss();
+
+			// set was not empty and there was a hit
+		} else {
+			if (RP == ReplacementPolicy.LRU)
+				tmpList[lastEntryTag].setLastAccessTime(accesses);
+			hit();
+		}
+	}
+
+	/**
+	 * 
+	 * @param tmpList
+	 * @param address
+	 * @return -1 if miss, index of block in tmpList otherwise
+	 */
+	private int isHit(MemoryAddress[] tmpList, MemoryAddress address) {
+		int index = EMPTY;
+		for (int i = 0; i < ASSOCIATIVITY; i++) {
+			if (tmpList[i] == null)
+				break;
+			if (getTag(tmpList[i]) == getTag(address)) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+
+	private void replace(MemoryAddress[] tmpList, MemoryAddress address) {
+		boolean isFull = tmpList[ASSOCIATIVITY - 1] != null;
+		int index = 0;
+
+		if (!isFull) {
+			// find first empty block and set replace index
+			for (int i = 0; i < ASSOCIATIVITY; i++) {
+				if (tmpList[i] == null) {
+					index = i;
+					break;
 				}
 			}
-			tmpList[index] = address;
-			miss();
+			// find oldest access time if full
+			// else put into first empty block
+		} else if (RP == ReplacementPolicy.LRU) {
+			// replace last used
+			for (int i = 0; i < ASSOCIATIVITY; i++) {
+				if (tmpList[i].getLastAccessTime() > tmpList[index].getLastAccessTime()) {
+					index = i;
+				}
+			}
+		} else if (RP == ReplacementPolicy.FIFO) {
+			// find oldest block and set replace index
+			for (int i = 0; i < ASSOCIATIVITY; i++) {
+				if (tmpList[i].getLastAccessTime() > tmpList[index].getLastAccessTime()) {
+					index = i;
+				}
+			}
 		}
+		tmpList[index] = address;
 	}
 
 	public String getLastTag() {
@@ -88,12 +105,6 @@ public class Cache extends BaseCache {
 
 	public String getLastBlock() {
 		return Integer.toHexString(lastBlockAddress);
-	}
-
-	public String getLastEntryTag() {
-		if (lastEntryTag == EMPTY)
-			return "";
-		return Integer.toHexString(lastEntryTag);
 	}
 
 	public String getLastEntryTags() {
